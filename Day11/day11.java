01598e53c9fc533d26ad6b4ac5b74e531f454e2a
Martins <olatunbosunmartins14@gmail.com>
2025-11-28 02:18:52 +0100

    day 11 java

--- Day11/day11.java
+++ Day11/day11.java
@@ -1,13 +1,25 @@
 public class Day11{
-    public  string getHighestNumber(String numbers){
-        String highestNumbers = "";
-        String[] numArray = numbers.split
-        for(String num : numArray){
-            if(Integer.parseInt(num) > highestNumber.length() || 
-               (num.length() == highestNumber.length() && num.compareTo(highestNumber) > 0)){
-                highestNumber = num;
+    public static void main(String[] args){
+        System.out.print(getHighestAndLowestNumber("-3 4 -9 64 3 42 9 1"));
+    }
+    public static String getHighestAndLowestNumber(String numbers){
+        String highestNumber = "";
+        String lowestNumber = "";
+        String[] numArray = numbers.split(" ");
+        int highestInt = Integer.parseInt(numArray[0]);
+        int lowestInt = Integer.parseInt(numArray[0]);
+        for(int index = 0; index < numArray.length; index++){
+            if(Integer.parseInt(numArray[index]) > highestInt){
+                highestInt = Integer.parseInt(numArray[index]);
+               highestNumber = numArray[index];
             }
+            
+            if(Integer.parseInt(numArray[index]) < lowestInt){
+                lowestInt = Integer.parseInt(numArray[index]);
+                lowestNumber = numArray[index];
+            }
+        }
+        return lowestNumber + " " + highestNumber;
         }
     }
 
-}
